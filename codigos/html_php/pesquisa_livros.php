3d4b2dba7887031171ecd7f4c12d02a717143cfc
TarcisioSLima <tarcisio.pesquisa.estudo@gmail.com>
2023-07-31 19:20:43 -0300

    auteração quase nula teste 2

--- codigos/html_php/pesquisa_livros.php
+++ codigos/html_php/pesquisa_livros.php
@@ -381,7 +381,12 @@ if (mysqli_num_rows($pesquisa_por_editora) > 0){
 }
 
 ?>
+
 </div>
+
+<script>
+    
+</script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
 </body>
